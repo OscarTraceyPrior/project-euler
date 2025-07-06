d570f15b500be234a507a644ebde77069f8219b5
OscarTraceyPrior <oscartraceyprior@gmail.com>
2025-07-06 21:37:17 +0100

    euler 15

---
+++ Euler15.java
@@ -0,0 +1,9 @@
+public class Euler15 { 
+
+
+    public static void main(String[] args...) {
+        System.out.println("This is a combinatorics problem, I've given the answer already")
+    }
+
+
+}
